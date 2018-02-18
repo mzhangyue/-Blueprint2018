4467db5df1f5363c0714dddcc6e6a177ff46f36c
mzhangyue <mzhang.zhang20@gmail.com>
2018-02-18 11:44:05 -0500

    Update Activities.py

--- Activities.py
+++ Activities.py
@@ -1,3 +1,13 @@
-activity_name = input(
-time
-day
+activities = {}
+user_input = input("Do you want to enter an activity? y/n ")
+while user_input != "n":
+    activity_name = input("What is the name of your activity? ") # Sleep
+    time = input("How long does it take in hours? ") # In hours
+    start = input("When does it start? ") # Military time
+    #interval = input("How many days between each activity? ") # In days
+    #activities.update({time: [activity_name, start, interval]})
+    time_paradox_check(activities)
+    activities.update({start: [activity_name, time]})
+    user_input = input("Do you want to enter an activity? y/n ")
+'''for start_time in activities:
+    print(activities[start_time])'''
