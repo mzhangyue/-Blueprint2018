d28bf4356709aba477a69df10b845ca06c235c39
mdas64 <36569640+mdas64@users.noreply.github.com>
2018-02-18 11:43:14 -0500

    Update scheduleFunctions.py

--- scheduleFunctions.py
+++ scheduleFunctions.py
@@ -5,9 +5,9 @@ schedule = {0:"",1:"",2:"",3:"",4:"",5:"",6:"",7:"",8:"",9:"",10:"",11:"",12:"",
 def activityPlanner(activity_name, amt_of_time, start_time):
     schedule[start_time] = activity_name
     while amt_of_time > 1:
-        start_time++
+        start_time += 1
         schedule[start_time] = activity name
-        amt_of_time--
+        amt_of_time -= 1
     
     
     
