1dd26c6273b682f7385d950ac5c1e2f217c28c53
mzhangyue <mzhang.zhang20@gmail.com>
2018-02-18 14:26:22 -0500

    Add files via upload

--- Activities.py
+++ Activities.py
@@ -1,3 +1,7 @@
+def time_paradox_check(start, activities):
+    for activity in activities: # For each activity (start: [activity_name, time])
+        if
+
 activities = {}
 user_input = input("Do you want to enter an activity? y/n ")
 while user_input != "n":
@@ -6,7 +10,7 @@ while user_input != "n":
     start = input("When does it start? ") # Military time
     #interval = input("How many days between each activity? ") # In days
     #activities.update({time: [activity_name, start, interval]})
-    time_paradox_check(activities)
+    time_paradox_check(start, activities)
     activities.update({start: [activity_name, time]})
     user_input = input("Do you want to enter an activity? y/n ")
 '''for start_time in activities:
